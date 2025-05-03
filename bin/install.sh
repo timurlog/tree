df8e79d98e9cba627893d96b52799a58fc7458bf
tilogie <tilogie@student.s19.be>
2025-05-03 21:37:51 +0200

    update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -12,6 +12,7 @@ RESET="\033[0m"
 REPO_URL="https://github.com/timurlog/tree.git"
 TEMP_DIR="$HOME/temp_____"
 INSTALL_DIR="$HOME/tree"
+HOME_DIR="$HOME"
 RC_FILE="$HOME/.zshrc"
 
 echo -e "${BLUE}Bienvenue dans l'installateur de Tree.${RESET}"
@@ -32,7 +33,7 @@ git clone --recursive "$REPO_URL" "$TEMP_DIR/tree" > /dev/null 2>&1 || { echo -e
 # Copie des fichiers dans le répertoire d'installation
 echo -e "${YELLOW}Copie des fichiers...${RESET}"
 rm -rf "$INSTALL_DIR"
-cp -r "$TEMP_DIR/tree" "$INSTALL_DIR" || { echo -e "${RED}Échec de la copie des fichiers.${RESET}"; exit 1; }
+cp -r "$TEMP_DIR/tree" "$HOME_DIR" || { echo -e "${RED}Échec de la copie des fichiers.${RESET}"; exit 1; }
 
 # Construction du projet
 echo -e "${YELLOW}Construction de Tree...${RESET}"
@@ -42,7 +43,7 @@ make > /dev/null 2>&1 || { echo -e "${RED}Échec de la construction.${RESET}"; e
 # Ajout de l'alias dans le fichier de configuration
 echo -e "${YELLOW}Ajout de l'alias dans ${MAGENTA}$RC_FILE${RESET}..."
 if ! grep -q "alias tree=" "$RC_FILE"; then
-    printf "\nalias tree=%s/tree/tree\n" "$INSTALL_DIR" >> "$RC_FILE" || { echo -e "${RED}Échec de la mise à jour de ${RC_FILE}.${RESET}"; exit 1; }
+    printf "\nalias tree=%s/tree/tree\n" "$HOME_DIR" >> "$RC_FILE"
 fi
 
 cd "$HOME" || { echo -e "${RED}Impossible de revenir au répertoire personnel.${RESET}"; exit 1; }

e89584ee52cc47eebdc1595c06d8cf341a0bcebc
Timur Logie <timur.logie@gmail.com>
2025-05-04 02:08:03 +0200

    update fix

--- src/format.c
+++ src/format.c
@@ -6,7 +6,7 @@
 /*   By: tilogie <tilogie@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 01:25:59 by tilogie           #+#    #+#             */
-/*   Updated: 2025/05/04 01:59:08 by tilogie          ###   ########.fr       */
+/*   Updated: 2025/05/04 02:06:52 by tilogie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void	tree_format(const char *str)
 {
+	char *user;
+
+	user = getenv("USER");
 	if (!strcmp(str, "-version"))
 	{
 		printf("tree version 0.1.1\n");
@@ -32,7 +35,21 @@ void	tree_format(const char *str)
 	}
 	else if (!strcmp(str, "-update"))
 	{
-		system("bash bin/update.sh");
+		if (user)
+		{
+			char command[256];
+			snprintf(command, sizeof(command), "bash /home/%s/tree/bin/update.sh", user);
+			if (system(command) == -1)
+			{
+				fprintf(stderr, "Error: Failed to execute update script.\n");
+				printf("Use this: bash -c \"$(curl -fsSL https://raw.github.com/timurlog/tree/main/bin/update.sh)\"\n");
+				exit(1);
+			}
+		}
+		else
+		{
+			fprintf(stderr, "Error: Unable to determine the user.\n");
+		}
 		exit(1);
 	}
 	else
